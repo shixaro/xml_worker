595441708c864dda885b52c01b12c4d8f2a80b47
shixaro <33911713+shixaro@users.noreply.github.com>
2022-11-05 17:40:25 +0400

    Update create_xml.py

--- create_xml.py
+++ create_xml.py
@@ -1,3 +1,4 @@
+import os
 import configparser
 from sources.xml_worker import XML_Worker
 
@@ -7,7 +8,8 @@ def main():
     config = configparser.RawConfigParser()
     config.read(ConfigFile)
     xml_worker = XML_Worker(config)    
- 
+    os.mkdir(config.get("MAIN", "XML_FILES_DIR")) if not os.path.exists(config.get("MAIN", "XML_FILES_DIR")) else None
+
     for x in range(int(config.get("MAIN", "ZIP4CREATE"))):
         files = xml_worker._generate_xml_files()
         xml_worker._archive_xml_files("{}.zip".format(x), files) 
